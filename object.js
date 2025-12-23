05a033095a637045c6f16bfbf8da3fdf3c6575ac
Abhishek2122-star <abhisheklad963@gmail.com>
2025-12-23 14:30:41 +0530

    loop file added

--- object.js
+++ object.js
@@ -1,12 +1,23 @@
+// const person = {
+//     firstName :'Abhishek',
+//     lastName : 'Lad',
+//     age : 50,
+//     fullName:function() {
+//         return this.firstName + " " + this.lastName;
+
+//     }
+// };
+
+// console.log(person.fullName())
+
 const person = {
     firstName :'Abhishek',
     lastName : 'Lad',
     age : 50,
-    fullName:function() {
+    get fullName() {
         return this.firstName + " " + this.lastName;
 
     }
 };
 
-console.log(person.fullName())
-
+console.log(person.fullName);
